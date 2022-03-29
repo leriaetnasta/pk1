901c1fbd550e74332189c0514f139e58dc9f17ad
leriaetnasta <61632665+leriaetnasta@users.noreply.github.com>
2022-03-29 09:23:30 +0000

    commit

--- src/Englobante.java
+++ src/Englobante.java
@@ -1,20 +1,25 @@
 public class Englobante {
     private Interne interne = new Interne();
     private int i= 5;
+    private int k= 3;
     public void afficher(){
-        System.out.println("i : "+ i);
+        System.out.println("i : "+ i + " K de la classe globante " + k);
     }
     public class Interne{
         private int k=i;
         public void afficher(){
-            System.out.println(this);
-            System.out.println(Englobante.this);
+            System.out.println(" k de la classe interne "  + k);
+            Englobante.this.afficher();
+        }
+        public static void teststatic(){
+            System.out.println("testing a static class in an innner class");
         }
     }
 
     public static void main(String[] args) {
         Englobante englobante = new Englobante();
         Englobante.Interne interne = englobante.new Interne();
+        englobante.afficher();
         interne.afficher();
     }
 }
